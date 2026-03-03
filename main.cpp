0c62e117440711600c68b62cf987e8638552c564
DedAlexevich <kuznecovpetr502@yandex.ru>
2026-03-03 16:59:42 +0300

    declared simplified list interface functions

--- main.cpp
+++ main.cpp
@@ -7,6 +7,39 @@ struct BiList {
   BiList< T >* prev;
 };
 
+template< class T >
+BiList< T >* makeBiList(T val);
+
+template< class T >
+BiList< T >* pushFront(BiList< T >* h);
+
+template< class T >
+BiList< T >* pushBack(BiList< T >* h);
+
+template < class T >
+BiList< T >* insert(BiList< T >* h ,size_t pos, T val);
+
+template< class T >
+size_t size(BiList< T >* h);
+
+template< class T >
+void clear(BiList< T >* h);
+
+template< class T >
+void popBack(BiList< T >* h);
+
+template< class T >
+void popFront(BiList< T >* h);
+
+template< class T, class R >
+R traverse(R r, BiList< T >* h, BiList< T >* e);
+
+template< class T, class R >
+R rtraverse(R r, BiList< T >* h, BiList< T >* e);
+
+template< class T >
+T& at(BiList< T >* h, size_t pos);
+
 int main()
 {}
 
